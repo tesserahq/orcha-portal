5870b3977a3da2b67888534655b81ead22bb60c5
yayakyogi <yayaktaka@gmail.com>
2026-01-26 00:29:13 +0700

    [FIXES #28]: Improve zoom control in dark mode

--- app/components/react-flow/nodes/property.tsx
+++ app/components/react-flow/nodes/property.tsx
@@ -24,8 +24,6 @@ export default function NodeProperty({
   eventTypes,
   isLoading,
 }: IProps) {
-  console.log('property', property)
-
   return (
     <>
       <Label className="flex items-center gap-2">
